5e3c34a3245bbaa1c8fdaf876ab0a6544d4232ef
Jorge Gonzalez <jorgegfx@gmail.com>
2024-07-03 15:53:51 -0400

    OpenGad

--- src/main/scala/com/jworkdev/trading/robot/data/signals/OpenGapSignalFinder.scala
+++ src/main/scala/com/jworkdev/trading/robot/data/signals/OpenGapSignalFinder.scala
@@ -5,7 +5,7 @@ import com.jworkdev.trading.robot.data.strategy.opengap.OpenGapSignalInput
 class OpenGapSignalFinder extends SignalFinder[OpenGapRequest]:
 
   private def getPercentageChange(before: Double,
-                                  after: Double): Double = Math.abs(before - after)/before
+                                  after: Double): Double = Math.abs(before - after)/before * 100
 
   private def isInRange(before: Double,
                         after: Double): Boolean = {
@@ -25,12 +25,13 @@ class OpenGapSignalFinder extends SignalFinder[OpenGapRequest]:
       val entrySignal = input.currentPrices.headOption.map(entryPrice=>{
         Signal(date=entryPrice.snapshotTime,`type`=entrySignalType,stockPrice = entryPrice)
       })
+      val exitSignalType = if(entrySignalType == SignalType.Buy) SignalType.Sell else SignalType.Buy
       val exitSignal = input.currentPrices.findLast(exitPrice=>{
         val currentGap = Math.abs(input.closingPrice - exitPrice.close)
         val percentage = getPercentageChange(before = gap, after = currentGap)
         percentage < 80
       }).map(exitPrice=>{
-        Signal(date=exitPrice.snapshotTime,`type`=entrySignalType,stockPrice = exitPrice)
+        Signal(date=exitPrice.snapshotTime,`type`=exitSignalType,stockPrice = exitPrice)
       })
       List(entrySignal,exitSignal).flatten
     })
