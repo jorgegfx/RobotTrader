acd342ce35664858e8168135c9b5e7bcafbf236b
Jorge Gonzalez <jorgegfx@gmail.com>
2024-07-06 08:19:17 -0400

    Zio Tests

--- src/test/scala/com/jworkdev/trading/robot/service/TradingExecutorServiceSpec.scala
+++ src/test/scala/com/jworkdev/trading/robot/service/TradingExecutorServiceSpec.scala
@@ -1,35 +1,45 @@
 package com.jworkdev.trading.robot.service
 
-import com.jworkdev.trading.robot.config.StrategyConfigurations
+import com.jworkdev.trading.robot.config.{MACDStrategyConfiguration, StrategyConfigurations}
+import com.jworkdev.trading.robot.data.strategy.macd.{MACDMarketDataStrategyRequest, MACDMarketDataStrategyResponse}
 import com.jworkdev.trading.robot.data.strategy.{MarketDataStrategyProvider, MarketDataStrategyRequest, MarketDataStrategyRequestFactory, MarketDataStrategyResponse}
 import com.jworkdev.trading.robot.domain.{FinInstrumentConfig, FinInstrumentType, TradingStrategyType}
+import com.jworkdev.trading.robot.market.data.SnapshotInterval.OneMinute
+import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar.mock
 import zio.test.{Spec, ZIOSpecDefault, assertTrue}
 
+import scala.util.Success
+
 object TradingExecutorServiceSpec extends ZIOSpecDefault:
-  private val marketDataStrategyProvider: MarketDataStrategyProvider[MarketDataStrategyRequest, MarketDataStrategyResponse]
-  = mock[MarketDataStrategyProvider[MarketDataStrategyRequest, MarketDataStrategyResponse]]
-  private val marketDataStrategyRequestFactory: MarketDataStrategyRequestFactory =
-    mock[MarketDataStrategyRequestFactory]
-  def spec: Spec[Any, Throwable] = suite("testExecute") {
-    val tradingExecutorService =
-      new TradingExecutorServiceImpl(
-        marketDataStrategyProvider = marketDataStrategyProvider,
-        marketDataStrategyRequestFactory = marketDataStrategyRequestFactory
-      )
-    val balancePerFinInst = 1000
-    val symbol = "NVDA"
-    val finInstrumentConfigs = List(
-      FinInstrumentConfig(
-        symbol = symbol,
-        pnl = None,
-        strategy = TradingStrategyType.MACD,
-        finInstrumentType = FinInstrumentType.Stock,
-        lastPnlUpdate = None
-      )
+  private val marketDataStrategyProvider
+      : MarketDataStrategyProvider[MarketDataStrategyRequest, MarketDataStrategyResponse] =
+    mock[MarketDataStrategyProvider[MarketDataStrategyRequest, MarketDataStrategyResponse]]
+  private val marketDataStrategyRequestFactory: MarketDataStrategyRequestFactory = MarketDataStrategyRequestFactory()
+  val tradingExecutorService =
+    new TradingExecutorServiceImpl(
+      marketDataStrategyProvider = marketDataStrategyProvider,
+      marketDataStrategyRequestFactory = marketDataStrategyRequestFactory
     )
-    val strategyConfigurations = StrategyConfigurations()
-    test("executes a buy when no openPositions") {
+  val balancePerFinInst = 1000
+  val symbol = "NVDA"
+  def spec: Spec[Any, Throwable] = suite("testExecute")(
+    test("No prices, No signals found, empty orders") {
+      val finInstrumentConfigs = List(
+        FinInstrumentConfig(
+          symbol = symbol,
+          pnl = None,
+          strategy = TradingStrategyType.MACD,
+          finInstrumentType = FinInstrumentType.Stock,
+          lastPnlUpdate = None
+        )
+      )
+      val macdMarketDataStrategyResponse = MACDMarketDataStrategyResponse(prices = List.empty)
+      val strategyConfigurations =
+        StrategyConfigurations(macd = Some(MACDStrategyConfiguration(snapshotInterval = OneMinute)))
+      when(marketDataStrategyProvider.provide(request =
+        MACDMarketDataStrategyRequest(symbol = symbol, snapshotInterval = OneMinute))).
+        thenReturn(Success(macdMarketDataStrategyResponse))
       for
         orders <- tradingExecutorService.execute(
           balancePerFinInst = balancePerFinInst,
@@ -37,6 +47,31 @@ object TradingExecutorServiceSpec extends ZIOSpecDefault:
           openPositions = List.empty,
           strategyConfigurations = strategyConfigurations
         )
-      yield assertTrue(orders.nonEmpty)
-    }
-  }
+      yield assertTrue(orders.isEmpty)
+    },
+    test("No signals found, empty orders") {
+      val finInstrumentConfigs = List(
+        FinInstrumentConfig(
+          symbol = symbol,
+          pnl = None,
+          strategy = TradingStrategyType.MACD,
+          finInstrumentType = FinInstrumentType.Stock,
+          lastPnlUpdate = None
+        )
+      )
+      val macdMarketDataStrategyResponse = MACDMarketDataStrategyResponse(prices = List.empty)
+      val strategyConfigurations =
+        StrategyConfigurations(macd = Some(MACDStrategyConfiguration(snapshotInterval = OneMinute)))
+      when(marketDataStrategyProvider.provide(request =
+        MACDMarketDataStrategyRequest(symbol = symbol, snapshotInterval = OneMinute))).
+        thenReturn(Success(macdMarketDataStrategyResponse))
+      for
+        orders <- tradingExecutorService.execute(
+          balancePerFinInst = balancePerFinInst,
+          finInstrumentConfigs = finInstrumentConfigs,
+          openPositions = List.empty,
+          strategyConfigurations = strategyConfigurations
+        )
+      yield assertTrue(orders.isEmpty)
+    })
+
