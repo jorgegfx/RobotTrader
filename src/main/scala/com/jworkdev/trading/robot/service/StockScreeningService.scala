335966cd60c3794d9a23280314d66a3d05aa83be
JORGE GONZALEZ <jorgegfx@gmail.com>
2024-07-10 21:14:47 -0400

    Group Batches

--- src/main/scala/com/jworkdev/trading/robot/service/StockScreeningService.scala
+++ src/main/scala/com/jworkdev/trading/robot/service/StockScreeningService.scala
@@ -1,5 +1,6 @@
 package com.jworkdev.trading.robot.service
 
+import com.jworkdev.trading.robot.domain
 import com.jworkdev.trading.robot.domain.FinInstrument
 import com.jworkdev.trading.robot.domain.FinInstrumentType.Stock
 import com.jworkdev.trading.robot.market.data.SnapshotInterval.SixtyMinutes
@@ -7,39 +8,52 @@ import com.jworkdev.trading.robot.market.data.{ExchangeDataProvider, MarketDataP
 import zio.{Task, ZIO, ZLayer}
 
 import java.time.Instant
+import scala.util.Try
 
 trait StockScreeningService:
   def screenFinInstruments(): Task[List[FinInstrument]]
 
 class StockScreeningServiceImpl(
     private val exchangeDataProvider: ExchangeDataProvider,
-    private val marketDataProvider: MarketDataProvider
+    private val marketDataProvider: MarketDataProvider,
+    private val parallelismLevel: Int = 5
 ) extends StockScreeningService:
+  private val exchange = "NASDAQ"
+  private val finInstrumentType: domain.FinInstrumentType = Stock
+
+  private def distributeInGroups[T](list: List[T], n: Int): List[List[T]] = {
+    val groupedList = list.zipWithIndex.groupBy(_._2 % n).values.map(_.map(_._1).toList).toList
+    groupedList
+  }
+
   override def screenFinInstruments(): Task[List[FinInstrument]] = for
-    symbols <- exchangeDataProvider.findAllSymbols("NASDAQ", Stock)
-    fibers <- ZIO.foreach(symbols) { symbol =>
-      buildFinInstrument(symbol = symbol).fork
+    symbols <- exchangeDataProvider.
+      findAllSymbols(exchange = exchange, finInstrumentType = finInstrumentType)
+    batches <- ZIO.succeed(distributeInGroups(list = symbols, parallelismLevel))
+    fibers <- ZIO.foreach(batches) { symbols =>
+      buildFinInstruments(symbols = symbols).fork
     }
     results <- ZIO.foreach(fibers)(_.join)
-  yield results
+  yield results.flatten
 
-  private def buildFinInstrument(symbol: String): Task[FinInstrument] =
+  private def buildFinInstruments(symbols: List[String]):Task[List[FinInstrument]] =
+    ZIO.attemptBlocking(symbols.flatMap(buildFinInstrument))
+
+  private def buildFinInstrument(symbol: String): Option[FinInstrument] =
     calculateVolatility(symbol = symbol).map(volatility =>
       FinInstrument(
         symbol = symbol,
-        `type` = Stock,
+        `type` = finInstrumentType,
         volatility = volatility,
-        exchange = "NASDAQ",
+        exchange = exchange,
         creationDate = Instant.now()
       )
-    )
+    ).toOption
 
-  private def calculateVolatility(symbol: String): Task[Double] =
-    ZIO.attemptBlocking(
-      marketDataProvider
-        .getIntradayQuotesDaysRange(symbol = symbol, interval = SixtyMinutes, daysRange = 7)
-        .fold(ex => 0d, VolatilityCalculator.calculate)
-    )
+  private def calculateVolatility(symbol: String): Try[Double] =
+    marketDataProvider
+      .getIntradayQuotesDaysRange(symbol = symbol, interval = SixtyMinutes, daysRange = 7).
+        map(VolatilityCalculator.calculate)
 
 object StockScreeningService:
   val layer: ZLayer[ExchangeDataProvider, Nothing, StockScreeningService] =
