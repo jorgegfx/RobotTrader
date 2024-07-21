0df620e22a6fbf51ec0e749d7409d1fdc01d67db
JORGE GONZALEZ <jorgegfx@gmail.com>
2024-07-21 09:40:37 -0400

    SQL Schema

--- schema.sql
+++ schema.sql
@@ -1,4 +1,62 @@
-use `robot_trading`;
+-- MySQL dump 10.13  Distrib 8.4.1, for macos14 (x86_64)
+--
+-- Host: localhost    Database: robot_trading
+-- ------------------------------------------------------
+-- Server version	8.4.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `account`
+--
+
+DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `account` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `balance` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `fin_instrument`
+--
+
+DROP TABLE IF EXISTS `fin_instrument`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fin_instrument` (
+  `symbol` varchar(20) NOT NULL,
+  `name` varchar(250) DEFAULT NULL,
+  `type` varchar(10) NOT NULL,
+  `volatility` double DEFAULT NULL,
+  `exchange` varchar(20) NOT NULL,
+  `creation_date` datetime NOT NULL,
+  `last_update` datetime DEFAULT NULL,
+  `active` varchar(3) DEFAULT NULL,
+  PRIMARY KEY (`symbol`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `position`
+--
+
+DROP TABLE IF EXISTS `position`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `position` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `symbol` varchar(10) NOT NULL,
@@ -10,27 +68,46 @@ CREATE TABLE `position` (
   `pnl` double DEFAULT NULL,
   `tradingStrategyType` varchar(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `trading_exchange`
+--
 
-CREATE TABLE `account` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
-  `balance` double DEFAULT NULL,
+DROP TABLE IF EXISTS `trading_exchange`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `trading_exchange` (
+  `id` varchar(10) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `openingTime` time NOT NULL,
+  `closingTime` time NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE `fin_instrument` (
-  `symbol` varchar(10) NOT NULL,
-  `type` varchar(10) NOT NULL,
-  `volatility` double NOT NULL,
-  `exchange` varchar(20) NOT NULL,
-  `creation_date` datetime NOT NULL,
-  PRIMARY KEY (`symbol`)
-) ENGINE=InnoDB;
+--
+-- Table structure for table `trading_strategy`
+--
 
+DROP TABLE IF EXISTS `trading_strategy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `trading_strategy` (
   `type` varchar(10) NOT NULL,
   `pnl` double DEFAULT NULL,
   PRIMARY KEY (`type`)
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-07-21  9:40:02
