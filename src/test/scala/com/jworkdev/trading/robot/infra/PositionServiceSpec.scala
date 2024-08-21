18af8028d877c645816dad7f53ea2714d1b3751d
JORGE GONZALEZ <jorgegfx@gmail.com>
2024-08-21 12:31:24 -0400

    DB Tests

--- src/test/scala/com/jworkdev/trading/robot/infra/PositionServiceSpec.scala
+++ src/test/scala/com/jworkdev/trading/robot/infra/PositionServiceSpec.scala
@@ -1,31 +1,21 @@
 package com.jworkdev.trading.robot.infra
 
+import com.jworkdev.trading.robot.Order
+import com.jworkdev.trading.robot.OrderType.{Buy, Sell}
 import com.jworkdev.trading.robot.domain.{Position, TradingStrategyType}
 import com.jworkdev.trading.robot.service.PositionService
-import com.jworkdev.trading.robot.service.TradingExecutorServiceSpec.{suite, test}
-import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
-import doobie.util.log.LogHandler
-import doobie.util.transactor
-import doobie.util.transactor.Transactor
 import io.github.gaelrenoux.tranzactio.doobie.*
-import io.github.gaelrenoux.tranzactio.{DatabaseOps, ErrorStrategies, ErrorStrategiesRef}
+import io.github.gaelrenoux.tranzactio.{DatabaseOps, ErrorStrategiesRef}
 import zio.*
 import zio.interop.catz.*
 import zio.test.{ZIOSpecDefault, test, *}
 
 import java.time.ZonedDateTime
-import javax.sql.DataSource
 
-object PositionServiceSpec extends ZIOSpecDefault {
-  private def hikariConfig(): HikariConfig =
-    val hikariConfig = new HikariConfig()
-    hikariConfig.setJdbcUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1")
-    hikariConfig.setUsername("sa")
-    hikariConfig.setPassword("")
-    hikariConfig.setDriverClassName("org.h2.Driver")
-    hikariConfig.setMaximumPoolSize(10)
-    hikariConfig
-  private val testPosition = Position(id = 0,
+object PositionServiceSpec extends ZIOSpecDefault:
+
+  private val testPosition = Position(
+    id = 0,
     symbol = "NVDA",
     numberOfShares = 2,
     openPricePerShare = 10,
@@ -33,49 +23,92 @@ object PositionServiceSpec extends ZIOSpecDefault {
     openDate = ZonedDateTime.now(),
     closeDate = None,
     pnl = None,
-    tradingStrategyType = TradingStrategyType.MACD)
-  val dbLayer: ZLayer[Any, Throwable, DataSource] = ZLayer.scoped {
-    for
-      ds <- ZIO.attemptBlocking { new HikariDataSource(hikariConfig()) }
-    yield ds
-  }
-  implicit val dbContext: DbContext =
-    DbContext(logHandler = LogHandler.jdkLogHandler[Task])
+    tradingStrategyType = TradingStrategyType.MACD
+  )
+
+  private val openPosition = testPosition.copy(symbol = "IBM1")
 
   val positionServiceLayer: ULayer[PositionService] =
     ZLayer.succeed(new PositionServiceImpl)
 
-  private val dbRecovery = ZLayer.succeed(
-    ErrorStrategies
-      .timeout(10.seconds)
-      .retryForeverExponential(10.seconds, maxDelay = 10.seconds)
-  )
-  private val datasource = dbRecovery >>> DatabaseConfig.layer
-  
-  val database: ZLayer[Any, Throwable, DatabaseOps.ServiceOps[
-    transactor.Transactor[Task]
-  ]] =
-    (datasource ++ dbRecovery) >>> Database.fromDatasourceAndErrorStrategies
-  val alternateDbRecovery: ErrorStrategies =
-    ErrorStrategies.timeout(10.seconds).retryCountFixed(3, 3.seconds)
-  
+  val positionServiceDllLayer: ULayer[PositionServiceDDL] =
+    ZLayer.succeed(new PositionServiceDDLImpl)
+
   private val appEnv =
-    DatabaseConfig.database ++ positionServiceLayer
+    H2Database.database ++ positionServiceLayer ++ positionServiceDllLayer
 
-  type AppEnv = Database & PositionService
-
-  implicit val errorRecovery: ErrorStrategiesRef =
-    DatabaseConfig.alternateDbRecovery
+  type AppEnv = Database & PositionService & PositionServiceDDL
 
   override def spec: Spec[Any, Throwable] = suite("testExecute")(
     test("findAll") {
-      val createAndGetPosition = for {
+      val createAndGetPosition = for
+        _ <- ZIO.serviceWithZIO[PositionServiceDDL](_.initialize())
         _ <- ZIO.serviceWithZIO[PositionService](_.create(position = testPosition))
         positions <- ZIO.serviceWithZIO[PositionService](_.findAll())
-      } yield positions.last
-      for 
-        position <- Database.transactionOrWiden(createAndGetPosition).provideLayer(appEnv)
-      yield assertTrue(position == testPosition)  
+      yield positions.last
+      for position <- Database.transactionOrWiden(createAndGetPosition).provideLayer(appEnv)
+      yield assertTrue(
+        position.symbol == testPosition.symbol &&
+          position.numberOfShares == testPosition.numberOfShares
+      )
+    },
+    test("findAllOpen") {
+      val createAndGetPosition = for
+        _ <- ZIO.serviceWithZIO[PositionServiceDDL](_.initialize())
+        _ <- ZIO.serviceWithZIO[PositionService](_.create(position = testPosition))
+        positions <- ZIO.serviceWithZIO[PositionService](_.findAllOpen())
+      yield positions.last
+      for position <- Database.transactionOrWiden(createAndGetPosition).provideLayer(appEnv)
+      yield assertTrue(
+        position.symbol == testPosition.symbol &&
+          position.numberOfShares == testPosition.numberOfShares
+      )
+    },
+    test("createOpenPositionsFromOrders") {
+      val createOpenPositionsFromOrders = for
+        _ <- ZIO.serviceWithZIO[PositionServiceDDL](_.initialize())
+        _ <- ZIO.serviceWithZIO[PositionService](_.createOpenPositionsFromOrders(orders = List(
+          Order(
+            `type` = Buy,
+            symbol = "IBM2",
+            dateTime = ZonedDateTime.now(),
+            shares = 2,
+            price = 20,
+            tradingStrategyType = TradingStrategyType.MACD,
+            positionId = None
+          )
+        )))
+        positions <- ZIO.serviceWithZIO[PositionService](_.findAllOpen())
+      yield positions
+      for position <- Database.transactionOrWiden(createOpenPositionsFromOrders).provideLayer(appEnv)
+        yield assertTrue(
+          position.exists(position=>position.symbol == "IBM2")
+        )
+    },
+    test("closeOpenPositions") {
+      val createAndClosePosition = for
+        _ <- ZIO.serviceWithZIO[PositionServiceDDL](_.initialize())
+        positionService <- ZIO.service[PositionService]
+        _ <- positionService.create(position = openPosition)
+        positions <- positionService.findAll()
+        count <- positionService.closeOpenPositions(
+          openPositions = positions,
+          orders = List(
+            Order(
+              `type` = Sell,
+              symbol = openPosition.symbol,
+              dateTime = ZonedDateTime.now(),
+              shares = 2,
+              price = 20,
+              tradingStrategyType = TradingStrategyType.MACD,
+              positionId = positions.find(position=>openPosition.symbol == position.symbol).map(_.id)
+            )
+          )
+        )
+      yield count
+      for count <- Database.transactionOrWiden(createAndClosePosition).provideLayer(appEnv)
+      yield assertTrue(
+        count == 1
+      )
     }
   )
-}
